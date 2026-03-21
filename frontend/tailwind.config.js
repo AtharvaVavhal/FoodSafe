c6382822bdac69af2b3727b34245584d3056611c
AtharvaVavhal <atharvavavhal@gmail.com>
2026-03-21 11:30:33 +0530

    UI

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50:  '#eaf3de',
+          100: '#c0dd97',
+          200: '#97c459',
+          400: '#639922',
+          600: '#3b6d11',
+          800: '#27500a',
+          900: '#173404',
+        }
+      },
+      fontFamily: {
+        sans: ['Noto Sans', 'sans-serif'],
+      }
+    }
+  },
+  plugins: []
+}
